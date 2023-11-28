43a8f0afef088734f2bf159fdb6cc853f0e8e7b5
Arnau-fg <a22arnfergil@inspedralbes.cat>
2023-11-28 13:19:17 +0100

    creat .vue partida

---
+++ vue-sockets/src/components/partida.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                
+            };
+        },
+        methods: {
+
+        },
+
+        mounted() {
+
+        },
+        created() {
+
+        },
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
