69c60eb4afcf2a22614b44cfb7bfad98e03f1804
Arnau-fg <a22arnfergil@inspedralbes.cat>
2023-11-29 13:05:45 +0100

    creat flux inicial

--- vue-sockets/src/components/partida.vue
+++ vue-sockets/src/components/partida.vue
@@ -1,30 +1,79 @@
 <template>
-    <div>
-
+    <div v-if="state.loading">
+        <div class="spinner-grow text-primary" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+    <div v-else>
+        <div>
+            <h2>{{ partida.data[partida.actual].pregunta }}</h2>
+        </div>
+        <span v-for="(resposta, index) in partida.data[partida.actual].parametres.respostes">
+            <Button :label="resposta" @click="respondre(index)"></Button> 
+        </span>
     </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                
-            };
+export default {
+    data() {
+        return {
+            constants: {
+                // API_URL: "https://api.github.com",
+                numRespostesTest: 4,
+            },
+            state: {
+                loading: true,
+                error: false,
+            },
+            partida: {
+                data: [],
+                actual: 0,
+            },
+            
+        };
+    },
+    methods: {
+        /**
+         * Fetches the data from the JSON file.
+         
+         */
+        fetchData() {
+            fetch("src/assets/json/rebre.json")
+                .then((response) => response.json())
+                .then((jsonData) => {
+                    this.partida.data = jsonData;
+                    this.state.loading = false;
+                })
+                .catch((error) => {
+                    console.error("Error fetching data:", error);
+                    this.state.loading = false;
+                    this.state.error = true;
+                });
         },
-        methods: {
 
-        },
+        respondre(index) {
+            if (index == 0) {
+                if (this.partida.actual  == this.partida.data.length - 1) {
+                    this.$router.push('/final');
+                } else {
+                    this.partida.actual++;
+                }
+            } else {
+                this.partida.actual = 0;
+            }
+        }
+    },
 
-        mounted() {
+    mounted() {
+        this.loading = true;
+        this.fetchData();
+    },
+    created() {
 
-        },
-        created() {
+    },
 
-        },
-        
-    }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
