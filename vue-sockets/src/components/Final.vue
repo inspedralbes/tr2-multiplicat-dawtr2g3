ccbdcde3c39c803f3055bf27f3032f789b280d35
Arnau-fg <a22arnfergil@inspedralbes.cat>
2023-12-05 08:10:19 +0100

    mostrar informacio final de partida

--- vue-sockets/src/components/Final.vue
+++ vue-sockets/src/components/Final.vue
@@ -1,17 +1,22 @@
 <template>
-
-YIPPIE
+  <h1>Final de la partida</h1>
+  <div class="jugador" v-for="(jugador, index) in players">
+    <span>Jugador: {{ jugador.nick }}</span><span>  Puntuació: {{ jugador.encertades }}</span>
+  </div>
 </template>
 <script>
-// import { useAppStore } from "../stores/app.js";
+import { useAppStore } from "../stores/app.js";
+import { computed } from 'vue';
+
 export default {
   data() {
+    const store = useAppStore();
     return {
-
+      players: computed(() => store.players),
     };
   },
   methods: {
-    
+
   },
 
   mounted() {
