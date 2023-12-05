f2d160bd34e7150d46957fc759d0bcc2deb24c0d
loriscrisafo <a22lorcrinor@inspedralbes.cat>
2023-12-05 12:28:36 +0100

    Canvi de documentacio

--- node/preguntes.js
+++ node/preguntes.js
@@ -1,6 +1,6 @@
 export async function fetchPreguntas() {
     //En local el fetch es fa a http://localhost:8000/api/preguntes
-    //En producció cambiem a ../laravel/public/api/preguntes
+    //En producció cambiem a http://mathroyale.daw.inspedralbes.cat/laravel/public/api/preguntes
     const response = await fetch('http://localhost:8000/api/preguntes');
     const data = await response.json();
     return data;
