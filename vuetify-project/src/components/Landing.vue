fa2c8620cdf9cad558b5663dc331f1d3a54e1c82
julsluks <a22betvilver@inspedralbes.cat>
2023-12-11 00:23:50 +0100

    disseny

--- vuetify-project/src/components/Landing.vue
+++ vuetify-project/src/components/Landing.vue
@@ -1,4 +1,6 @@
 <template>
+  <div>
+    <h1 class="text-h1">Math Royale</h1>
     <v-card class="ma-4 pa-4" width="400">
       <v-card-title>Landing Page</v-card-title>
       <v-card-item>
@@ -8,7 +10,22 @@
         <v-btn class="pa-2" @click="empezar">Jugar</v-btn>
       </v-card-actions>
     </v-card>
+  </div>
 </template>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    background: #46178f;
+  }
+
+  h1{
+    font-family: "Share Tech";
+  }
+  
+</style>
+
 <script>
 // import { useAppStore } from "../stores/app.js";
 import { socket } from '../socket';
@@ -23,7 +40,7 @@ export default {
   },
   methods: {
     empezar(){
-      
+
       const store = useAppStore();
       console.log("empezar");
       let nick = document.getElementById("nomJugador").value;
