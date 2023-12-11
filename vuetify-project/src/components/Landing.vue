2cfd54529245380a3d013947c89ddefb149a0f1e
julsluks <a22betvilver@inspedralbes.cat>
2023-12-11 12:27:18 +0100

    card center buttons fail

--- vuetify-project/src/components/Landing.vue
+++ vuetify-project/src/components/Landing.vue
@@ -1,19 +1,27 @@
 <template>
   <div>
-    <h1 class="title-landing text-center">Math Royale</h1>
+    <h1 class="title-landing text-center mb-5">Math Royale</h1>
     <div class="text-center">
       <v-row>
-        <v-col class="text-center">
-          <v-card class="ma-4 pa-4" width="400">
+        <v-col class="text-center d-flex align-center justify-end">
+          <v-card class="mr-5 pa-4" width="400">
             <v-text-field class="pa-2" label="UserJugador" id="nomJugador"></v-text-field>
             <v-btn rounded color="#106b03" class="px-5 mr-5">Tutorial</v-btn>
             <v-btn rounded color="#fad09e" class="px-5 ml-2 text-white">Registre</v-btn>
             <v-btn rounded color="#f5a23d" class="px-5 ml-2 text-white">Login</v-btn>
           </v-card>
         </v-col>
-        <v-col class="text-center">
-          <v-btn rounded class="ma-4 text-black" color="#f5a23d" @click="empezar">Entrar</v-btn>
-          <v-btn rounded class="ma-4 text-black" color="#f5a23d">Crear</v-btn>
+        <v-col class="text-center d-flex align-center justify-start">
+          <v-row>
+            <v-col>
+              <v-btn rounded class="ma-4 text-black" color="#f5a23d" @click="empezar">Entrar</v-btn>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-btn rounded class="ma-4 text-black" color="#f5a23d">Crear</v-btn>
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
     </div>
@@ -23,7 +31,6 @@
 <style>
 * {
   /* background-color: #46178f; */
-  /* color: white; */
 }
 
 .title-landing {
@@ -33,6 +40,11 @@
   line-height: 6rem;
   text-transform: none !important;
   color: #ffc00a;
+}
+
+v-col {
+  margin: 0;
+  padding: 0;
 }
 </style>
 
