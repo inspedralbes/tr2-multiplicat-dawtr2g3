9827976475a83c752882245ca6500704d6836219
julsluks <a22betvilver@inspedralbes.cat>
2023-12-13 11:02:08 +0100

    a

--- vuetify-project/src/components/Landing.vue
+++ vuetify-project/src/components/Landing.vue
@@ -3,25 +3,30 @@
     <h1 class="title-landing text-center">Math Royale</h1>
     <div class="container">
       <div class="container__card card">
-        <v-card class="mr-5 pa-4" width="400">
-          <v-avatar image="../assets/Avatar_22.png" size="80">
-            <!-- <v-btn icon="$vuetify">
-              Button
-            </v-btn> -->
-          </v-avatar>
-          <v-text-field class="pa-2" label="UserJugador" id="nomJugador"></v-text-field>
-          <v-btn rounded color="#106b03" class="px-5 mr-5">Tutorial</v-btn>
-          <v-btn rounded color="#fad09e" class="px-5 ml-2 text-white">Registre</v-btn>
-          <v-btn rounded color="#f5a23d" class="px-5 ml-2 text-white">Login</v-btn>
+        <v-card class="pa-5" color="#864cbf" width="500">
+          <div class="card__items items">
+            <div class="items__avatar avatar">
+              <v-avatar class="avatar__image" image="../assets/Avatar_22.png" size="100"></v-avatar>
+              <v-btn class="avatar-refresh" size="x-small" icon>
+                <v-icon>mdi-refresh</v-icon>
+              </v-btn>
+            </div>
+            <div class="items__userName userName">
+              <v-text-field class="pa-2" label="UserJugador" id="nomJugador"></v-text-field>
+            </div>
+            <div class="items__btn-tutorial btn-tutorial">
+              <v-btn rounded color="#106b03" class="px-5 mr-5">Tutorial</v-btn>
+            </div>
+            <div class="items__btn-log-reg">
+              <v-btn rounded color="#fad09e" class="px-5 ml-2 text-white">Registre</v-btn>
+              <v-btn rounded color="#f5a23d" class="px-5 ml-2 text-white">Login</v-btn>
+            </div>
+          </div>
         </v-card>
       </div>
       <div class="container__buttons buttons">
-        <div class="buttons__entrar entrar">
-          <v-btn rounded="xl" class="btn__entrar" color="#f5a23d" size="x-large" @click="empezar">Entrar</v-btn>
-        </div>
-        <div class="buttons__crear crear"> 
-          <v-btn rounded="xl" class="btn__crear" color="#f5a23d" size="x-large">Crear</v-btn>
-        </div>
+        <v-btn rounded="xl" class="btn__entrar" color="#f5a23d" size="x-large" @click="empezar">Entrar</v-btn>
+        <v-btn rounded="xl" class="btn__crear" color="#f5a23d" size="x-large">Crear</v-btn>
       </div>
     </div>
   </div>
@@ -64,16 +69,75 @@
     display: flex;
     align-items: center;
     justify-content: center;
+    position: relative;
+    left: 30vh;
+  }
+
+  .card__items {
+    display: grid;
+    grid-template-columns: 0.5fr 1fr; 
+    grid-row-gap: 7%;
+    grid-template-areas: 
+    "avatar userName"
+    "tutorial logReg";
+  }
+
+  .items__avatar {
+    grid-area: avatar;
+    display: flex;
+    justify-content: center;
+    
+  }
+
+  .items__userName {
+    grid-area: userName;
+    display: flex;
+    justify-content: center;
+  }
+
+  .items__btn-tutorial {
+    grid-area: tutorial;
+    display: flex;
+    justify-content: start;
+  }
+  
+  .items__btn-log-reg {
+    grid-area: logReg;
+    display: flex;
+    justify-content: end;
   }
 
   .container__buttons {
-    display: grid;
-    align-items: center;
+    margin: auto;
+    height: 20vh;
+    width: 500px;
     justify-content: center;
+    align-items: center;
+    display: grid;
+    position: relative;
+    right: 20vh;
   }
 
   .btn__entrar, .btn__crear {
     font-size: 3rem !important;
+  }
+
+  @media screen and (max-width: 1350px) {
+    
+    .container {
+      width: 100vw;
+      height: 50vh;
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+    }
+
+    .container__card {
+      left: 10vh;
+    }
+
+    .container__buttons {
+      right: 10vh;
+    }
   }
 
 </style>
