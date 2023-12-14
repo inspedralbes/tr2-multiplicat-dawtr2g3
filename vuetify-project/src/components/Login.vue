64f447605916333ab9337587fd1c59dd8db6ff4d
SaraMartinez21 <a21sarmarbau@inspedralbes.cat>
2023-12-14 10:48:54 +0100

    register terminado

--- vuetify-project/src/components/Login.vue
+++ vuetify-project/src/components/Login.vue
@@ -53,6 +53,7 @@ body {
 .container-imagen {
   background-image: url('../assets/backgrounds/background-login.png');
   height: 100vh;
+  background-size: cover;
 }
 
 .input__text{
--- vuetify-project/src/components/Register.vue
+++ vuetify-project/src/components/Register.vue
@@ -1,32 +1,62 @@
 <template>
-  <v-card class="mx-auto" max-width="344" title="User Registration">
-    <v-container>
-      <v-text-field v-model="first" color="primary" label="First name" variant="underlined"></v-text-field>
+  <div class="container">
+    <div class="container__register">
+      <v-card class="mx-auto register" max-width="344" title="Registrar">
+        <v-container >
+          <v-text-field v-model="first" color="deep-purple-darken-1" label="First name" variant="solo" ></v-text-field>
 
-      <v-text-field v-model="last" color="primary" label="Last name" variant="underlined"></v-text-field>
+          <v-text-field v-model="last" color="deep-purple-darken-1" label="Last name" variant="solo"></v-text-field>
 
-      <v-text-field v-model="email" color="primary" label="Email" variant="underlined"></v-text-field>
+          <v-text-field v-model="email" color="deep-purple-darken-1" label="Email" variant="solo"></v-text-field>
 
-      <v-text-field v-model="password" color="primary" label="Password" placeholder="Enter your password"
-        variant="underlined"></v-text-field>
+          <v-text-field v-model="password" color="deep-purple-darken-1" label="Password" placeholder="Enter your password"
+            variant="solo"></v-text-field>
 
-      <v-checkbox v-model="terms" color="secondary" label="I agree to site terms and conditions"></v-checkbox>
-    </v-container>
-
-    <v-divider></v-divider>
-
-    <v-card-actions>
-      <v-spacer></v-spacer>
-
-      <v-btn color="success">
-        Complete Registration
-
-        <v-icon icon="mdi-chevron-right" end></v-icon>
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+          <v-btn class="button__register">Entrar</v-btn>
+        </v-container>
+      </v-card>
+    </div>
+    <div class="container__imgenRegister"></div>
+  </div>
 </template>
+<style scoped>
 
+.container {
+  width: 100vw;
+  height: 60vh;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+.container__register {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 90vh;
+  border-radius: 10px;
+
+}
+
+.container__imgenRegister{
+  background-image: url('../assets/backgrounds/background-register.png');
+  height: 100vh;
+  background-size: cover;
+}
+
+.register{
+  background-color: rgb(37, 7, 107,0.8);
+  height: 60vh;
+  border-radius: 6px;
+  width: clamp(48vh,50%,35vh);
+  color: aliceblue;
+}
+
+.button__register {
+  background-color: #33cccc;
+  position: static;
+  display: flex;
+  margin-left: 30vh;
+}
+</style>
 <script>
 export default {
   data: () => ({
