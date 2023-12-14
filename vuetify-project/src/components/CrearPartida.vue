d39113f8b1b19264c7a8c8e30c66c4f8d8bc5723
julsluks <a22betvilver@inspedralbes.cat>
2023-12-14 10:53:57 +0100

    add styles background

--- vuetify-project/src/components/CrearPartida.vue
+++ vuetify-project/src/components/CrearPartida.vue
@@ -1,17 +1,73 @@
 <template>
-    <div>
-        <div class="formPartida">
-            <label for="nom">Nom de la partida</label>
-            <input type="text" v-model="nom" name="nom">
-            <label for="maxJugadors">Màxim de jugadors</label>
-            <div class="slidecontainer">
-                <input type="range" min="2" max="40" value="20" class="slider" name="maxJugadors" v-model="maxJugadors" id="myRange">
+    <div class="page">
+        <div class="configuration-board">
+            <div class="configuration-title-background">
+                <h1 class="title"> Configura la teva partida</h1>
             </div>
+            <div class="configuration-buttons">
+                <label for="nom">Nom de la partida</label>
+                <input type="text" v-model="nom" name="nom">
+                <label for="maxJugadors">Màxim de jugadors</label>
+                <div class="slidecontainer">
+                    <input type="range" min="2" max="40" value="20" class="slider" name="maxJugadors" v-model="maxJugadors" id="myRange">
+                </div>
+            </div>
+            <button @click="crear">Crear partida</button>
         </div>
-        <button @click="crear">Crear partida</button>
-
     </div>
 </template>
+
+<style scoped>
+    .page{
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .configuration-board {
+        /* fons del container */
+        border-radius: 12px;
+        background-color: rgba(70, 23, 143, 0.7);
+        gap: 8px;
+        padding: 16px;
+        flex-wrap: nowrap;
+        box-shadow: inset 4px 4px 4px 0px rgba(0, 0, 0, 0.2), inset 4px 4px 4px 0px rgba(0, 0, 0, 0.2);
+
+        /* mida del container */
+        width: 70%;
+        height: 60%;
+
+        /* aliniació del container */
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-evenly;
+    }
+
+    .configuration-title-background {
+        width: 65vw;
+        height: 10vh;
+        border-radius: 50px;
+        background-color: rgb(226, 65, 4);
+    }
+
+    .title {
+        font-size: 5rem !important;
+        font-weight: 300;
+        line-height: 6rem;
+        text-transform: none !important;
+        color: #ffc00a;
+        margin-left: 2%;
+    }
+
+    .configuration-buttons {
+        
+    }
+
+</style>
+
 <script>
 // import { useAppStore } from "../stores/app.js";
 import { socket } from '../socket';
@@ -45,4 +101,3 @@ export default {
 
 }
 </script>
-  
