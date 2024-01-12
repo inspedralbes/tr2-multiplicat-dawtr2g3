a7ad42c33bb78e7a806513e67830b987f1a27345
loriscrisafo <123160689+loriscrisafo@users.noreply.github.com>
2024-01-12 08:59:59 +0100

    Videos metidos

--- vuetify-project/src/components/Tutorial.vue
+++ vuetify-project/src/components/Tutorial.vue
@@ -26,10 +26,7 @@
         <v-row>
             <v-col cols="12">
                 <div class="video-box">
-                    <video controls width="100%">
-                        <source src="https://www.youtube.com/watch?v=XNmIv_hYlik" type="video/mp4">
-                        Tu navegador no soporta el elemento de video.
-                    </video>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/XNmIv_hYlik?si=lQyB__dNN6QQCwbJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 </div>
             </v-col>
         </v-row>
@@ -52,10 +49,7 @@
         <v-row>
             <v-col cols="12">
                 <div class="video-box">
-                    <video controls width="100%">
-                        <source src="https://www.youtube.com/watch?v=K8eGJ-ZaONE" type="video/mp4">
-                        Tu navegador no soporta el elemento de video.
-                    </video>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/K8eGJ-ZaONE?si=Uh5Ncbbdf4WoRTIV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 </div>
             </v-col>
         </v-row>
@@ -66,10 +60,7 @@
             <v-col cols="12">
                 <h2 class="subtitle">Demostració de desafiar</h2>
                 <div class="video-box">
-                    <video controls width="100%">
-                        <source src="https://www.youtube.com/watch?v=R2oPLlv0Fc8" type="video/mp4">
-                        Tu navegador no soporta el elemento de video.
-                    </video>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/R2oPLlv0Fc8?si=ADSZGi-V1y19BhzQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 </div>
             </v-col>
         </v-row>
@@ -88,11 +79,58 @@
             </v-col>
         </v-row>
         <v-row>
-            <v-col cols="12" md="6" v-for="article in articles">
+            <v-col cols="12" md="6">
                 <v-card max-width="344" class="mx-auto article-card">
-                    <v-img cover height="200" :src="article.image"></v-img>
-                    <v-card-title>{{ article.title }}</v-card-title>
-                    <v-card-text>{{ article.content }}</v-card-text>
+                    <v-img cover height="200" src="/src/assets/poderes/curar.png"></v-img>
+                    <v-card-title>Curar</v-card-title>
+                    <v-card-text>Aquest poder et permet recuperar +15 punts de vida.</v-card-text>
+                </v-card>
+                <v-card max-width="344" class="mx-auto article-card">
+                    <v-img cover height="200" src="/src/assets/poderes/duelo.png"></v-img>
+                    <v-card-title>Desafiar</v-card-title>
+                    <v-card-text>L'item de desafiar es una de les parts importants del joc. Aquest item et permet desafiar a un altre jugador a un duel. Si guanyes el duel, el jugador perd 30 punts de vida. Si perds el duel, tu perds 15 punts de vida</v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="12" md="6"> 
+                <v-card max-width="344" class="mx-auto article-card">
+                    <v-img cover height="200" src="/src/assets/poderes/escudo.png"></v-img>
+                    <v-card-title>Escut</v-card-title>
+                    <v-card-text>El escut et permet protegir-te d'un error a l'ahora de respondre. Si en una ronda falles dos cops o més el primer cop no perdras punts de vida, per tant et protegira.</v-card-text>
+                </v-card>
+                <v-card max-width="344" class="mx-auto article-card">
+                    <v-img cover height="200" src="/src/assets/poderes/pararTiempo.png"></v-img>
+                    <v-card-title>Parar el temps</v-card-title>
+                    <v-card-text>L'accesori de parar el temps et permet parar el temps durant 5 segons. Això et permetrà tenir més temps per pensar la resposta correcta.</v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="12" md="6">
+                <v-card max-width="344" class="mx-auto article-card">
+                    <v-img cover height="200" src="/src/assets/poderes/reducirTiempo.png"></v-img>
+                    <v-card-title>Reduir el temps</v-card-title>
+                    <v-card-text>El poder de reduir el temps et permet reduir el temps de resposta a 5 segons d'un jugador que tu esculls. Es redueix el temps en la següent pregunta que el jugador seleccionat respongui. Això pot fer que el jugador no tingui temps suficient per pensar la resposta correcta.</v-card-text>
+                </v-card>
+                <v-card max-width="344" class="mx-auto article-card">
+                    <v-img cover height="200" src="/src/assets/poderes/robarPoder.png"></v-img>
+                    <v-card-title>Robar poder</v-card-title>
+                    <v-card-text>Aquest poder et permet "robar" un poder. Quan utilitzes aquest poder, et surt una petita pantalla amb tots el poders disponibles. Tu esculls un poder i aquest poder passa a ser teu.</v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="12" md="6">
+                <v-card max-width="344" class="mx-auto article-card">
+                    <v-img cover height="200" src="/src/assets/poderes/robarVida.png"></v-img>
+                    <v-card-title>Robar vida</v-card-title>
+                    <v-card-text>Robar vida et permet robar 10 punts de vida, aixo fa que recuperis vida. El poder funciona de la següent manera: durant les següents 3 preguntes que responguis correctament, recuperes 10 punts de vida per cada pregunta correcta.</v-card-text>
+                </v-card>
+                <v-card max-width="344" class="mx-auto article-card">
+                    <v-img cover height="200" src="/src/assets/poderes/saltar.png"></v-img>
+                    <v-card-title>Saltar pregunta</v-card-title>
+                    <v-card-text>Aquest poder et permet saltar una pregunta. Això fa que no perdis punts de vida si no saps la resposta correcta.</v-card-text>
                 </v-card>
             </v-col>
         </v-row>
@@ -168,42 +206,42 @@ export default {
                     id: 1,
                     title: "Curar",
                     content: "Aquest poder et permet recuperar +15 punts de vida.",
-                    image: "src/assets/poderes/curar.png"
+                    image: "/src/assets/poderes/curar.png"
                 }, {
                     id: 2,
                     title: "Desafiar",
                     content: "L'item de desafiar es una de les parts importants del joc. Aquest item et permet desafiar a un altre jugador a un duel. Si guanyes el duel, el jugador perd 30 punts de vida. Si perds el duel, tu perds 15 punts de vida",
-                    image: "src/assets/poderes/duelo.png"
+                    image: "/src/assets/poderes/duelo.png"
                 }, {
                     id: 3,
                     title: "Escut",
                     content: "El escut et permet protegir-te d'un error a l'ahora de respondre. Si en una ronda falles dos cops o més el primer cop no perdras punts de vida, per tant et protegira.",
-                    image: "src/assets/poderes/escudo.png"
+                    image: "/src/assets/poderes/escudo.png"
                 }, {
                     id: 4,
                     title: "Parar el temps",
                     content: "L'accesori de parar el temps et permet parar el temps durant 5 segons. Això et permetrà tenir més temps per pensar la resposta correcta.",
-                    image: "src/assets/poderes/pararTiempo.png"
+                    image: "/src/assets/poderes/pararTiempo.png"
                 }, {
                     id: 5,
                     title: "Reduir el temps",
                     content: "El poder de reduir el temps et permet reduir el temps de resposta a 5 segons d'un jugador que tu esculls. Es redueix el temps en la següent pregunta que el jugador seleccionat respongui. Això pot fer que el jugador no tingui temps suficient per pensar la resposta correcta.",
-                    image: "src/assets/poderes/reducirTiempo.png"
+                    image: "/src/assets/poderes/reducirTiempo.png"
                 }, {
                     id: 6,
                     title: "Robar poder",
                     content: 'Aquest poder et permet "robar" un poder. Quan utilitzes aquest poder, et surt una petita pantalla amb tots el poders disponibles. Tu esculls un poder i aquest poder passa a ser teu.',
-                    image: "src/assets/poderes/robarPoder.png"
+                    image: "/src/assets/poderes/robarPoder.png"
                 }, {
                     id: 7,
                     title: "Robar vida",
                     content: "Robar vida et permet robar 10 punts de vida, aixo fa que recuperis vida. El poder funciona de la següent manera: durant les següents 3 preguntes que responguis correctament, recuperes 10 punts de vida per cada pregunta correcta.",
-                    image: "src/assets/poderes/robarVida.png"
+                    image: "/src/assets/poderes/robarVida.png"
                 }, {
                     id: 8,
                     title: "Saltar pregunta",
                     content: "Aquest poder et permet saltar una pregunta. Això fa que no perdis punts de vida si no saps la resposta correcta.",
-                    image: "src/assets/poderes/saltar.png"
+                    image: "/src/assets/poderes/saltar.png"
                 }
             ]
         };
